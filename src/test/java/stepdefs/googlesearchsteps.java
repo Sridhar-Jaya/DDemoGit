9264264c3731066c11e322ff972a5ae9d6c9eebe
sri <sridharj.work@gmail.com>
2024-07-28 20:43:52 +0530

    quit

--- src/test/java/stepdefs/googlesearchsteps.java
+++ src/test/java/stepdefs/googlesearchsteps.java
@@ -22,7 +22,6 @@ public class googlesearchsteps {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.quit();
 	}
 
 	@And("user is on google search page")
